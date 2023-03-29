d3d85818f59dbb5c6f4af651ebcd7b33723ffde1
Kevin Smith <kevin341s@yahoo.com>
2023-03-29 12:16:20 -0400

    continue landing page
    //moved image to public folder
    //adjusted styling to center items and wrap text in the center
    //changed the background color to blck

--- src/pages/landingPage.tsx
+++ src/pages/landingPage.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import Link from "next/link";
 import Head from "next/head";
+import Image from "next/image";
 
 const LandingPage = () => {
   return (
@@ -10,22 +11,34 @@ const LandingPage = () => {
       <Head>
         <title>Fitness App</title>
       </Head>
-      <div className="container mx-auto">
-        {/* Add your landing page content and styling */}
-        <h1 className="text-4xl font-bold text-center">Shock Fitness</h1>
-        <div className="text-center mt-8">
-          <Link
-            href="/register"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4"
-          >
-            Register
-          </Link>
-          <Link
-            href="/login"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          >
-            Login
-          </Link>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-black">
+        <div className="ml-8">
+          <Image
+            src="/images/shockfitnesstransparent.png"
+            alt="Fitness App Logo"
+            width={600}
+            height={100}
+          />
+        </div>
+        <p className="flex flex-col items-center text-center mb-8 text-white ml-10 mr-10">
+          Track your workouts, connect with friends, and achieve your fitness
+          goals.
+        </p>
+        <div className="items-center">
+          <div className="text-center mt-8">
+            <Link
+              href="/register"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4"
+            >
+              Register
+            </Link>
+            <Link
+              href="/login"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Login
+            </Link>
+          </div>
         </div>
       </div>
     </>
