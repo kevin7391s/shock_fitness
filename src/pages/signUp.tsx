f24f1ebeef31f4c3ff08e37d0b176f8d77d27670
Kevin Smith <kevin341s@yahoo.com>
2023-03-30 18:05:01 -0400

    start building signup page
    basic block form and image

--- src/pages/signUp.tsx
+++ src/pages/signUp.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import Image from "next/image";
 
 function signUp() {
-  return <div>Hello</div>;
+  const handleSignup = () => {};
+  return (
+    <div
+      className="flex flex-col items-center h-screen bg-black "
+      style={{ backgroundColor: "#121212" }}
+    >
+      <div
+        className=" flex flex-col items-center relative w-4/5 h-4/5 mx-auto  overflow-hidden bg-gray-200  rounded-lg border-2 border-gray-600 shadow-lg cursor-pointer mt-20"
+        style={{ backgroundColor: "#424242" }}
+      >
+        <Image
+          src="/images/shockfitnesstransparent.png"
+          alt="Fitness App Logo"
+          className=""
+          width={600}
+          height={100}
+        />
+        <div className=""></div>
+      </div>
+    </div>
+  );
 }
 
 export default signUp;
