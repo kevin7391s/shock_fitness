efe19dd0d9030ad8848de277da8faf17a2fcd836
Kevin Smith <kevin341s@yahoo.com>
2023-04-05 23:38:50 -0400

    added login picture
    //added it to login page
    //removed lines going through register page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -20,26 +20,31 @@ function Login() {
     try {
       await signInWithEmailAndPassword(auth, email, password);
       router.push("/home");
-    } catch (error) {
-      setError(error.message);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        setError(error.message);
+      } else {
+        setError("An unknown error occurred");
+      }
     }
   };
   return (
     <div className="flex flex-col items-center h-screen bg-black">
       <div
-        className="flex flex-col relative w-4/5 h-4/5 mx-auto overflow-hidden bg-gray-200 rounded-lg border-2 border-gray-600 shadow-lg cursor-pointer mt-20"
+        className="flex flex-col relative w-3/5 h-3/5 mx-auto overflow-hidden bg-gray-200 rounded-lg border-2 border-gray-600 shadow-lg cursor-pointer mt-20"
         style={{ backgroundColor: "#424242" }}
       >
         <div className="flex flex-col items-center">
           <Image
-            src="/images/shockfitnesstransparent.png"
+            src="/images/login.png"
             alt="Fitness App Logo"
             className="mb-10 mt-10"
-            width={400}
+            width={150}
             height={80}
             priority={true}
           />
         </div>
+
         {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
         <form onSubmit={handleLogin} method="POST" className="ml-3 mr-3">
           <p className="ml-4 text-sm text-white mb-1">Email</p>
