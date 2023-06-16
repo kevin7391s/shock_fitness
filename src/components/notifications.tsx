45820041095b1aa3eec3cc43fc4913d75cc5f9d2
Kevin Smith <kevin341s@yahoo.com>
2023-06-16 17:06:43 -0400

    added dropdown menu to notifications icon

--- src/components/notifications.tsx
+++ src/components/notifications.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 
-function Notifications() {
+function NotificationDropdown() {
   const [open, setOpen] = useState(false);
 
   const handleOpen = () => {
@@ -9,22 +9,49 @@ function Notifications() {
   };
 
   return (
-    <div>
-      <NotificationsIcon
-        onClick={handleOpen}
-        style={{ color: "white", marginRight: "1em" }}
-      />
+    <div className="relative inline-block text-left">
+      <div>
+        <NotificationsIcon
+          className={`h-6 w-6 cursor-pointer ${
+            open ? "text-cyan-400" : "text-black"
+          }`}
+          onClick={handleOpen}
+        />
+      </div>
       {open && (
-        <div className="dropdown">
-          <ul>
-            <li>Notification 1</li>
-            <li>Notification 2</li>
-            <li>Notification 3</li>
-          </ul>
+        <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-gray-800">
+          <div
+            className="py-1"
+            role="menu"
+            aria-orientation="vertical"
+            aria-labelledby="options-menu"
+          >
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-white hover:bg-gray-700"
+              role="menuitem"
+            >
+              Notification 1
+            </a>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-white hover:bg-gray-700"
+              role="menuitem"
+            >
+              Notification 2
+            </a>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-white hover:bg-gray-700"
+              role="menuitem"
+            >
+              Notification 3
+            </a>
+          </div>
         </div>
       )}
     </div>
   );
 }
 
-export default Notifications;
+export default NotificationDropdown;
