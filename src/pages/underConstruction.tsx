dca481a16fc6b3a8f0ba24ad624402ab90c176ba
Kevin Smith <kevin341s@yahoo.com>
2023-06-21 14:13:07 -0400

    add image
    added construction worker image to folder plus file

--- src/pages/underConstruction.tsx
+++ src/pages/underConstruction.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import NavBar from "@/components/navbar";
 import Footer from "@/components/footer";
+import Image from "next/image";
 
 function UnderConstruction() {
   return (
@@ -9,6 +10,15 @@ function UnderConstruction() {
       style={{ backgroundColor: "#121212" }}
     >
       <NavBar />
+      <div className="">
+        <Image
+          src="/images/worker.png"
+          alt="Worker"
+          className="relative "
+          width={500}
+          height={1000}
+        />
+      </div>
       <div className="text-center">
         <h1 className="text-5xl font-bold mt-10">Under Construction</h1>
         <p className="text-xl mt-5">
