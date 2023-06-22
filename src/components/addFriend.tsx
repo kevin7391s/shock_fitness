d4ed0aa0057f95c95ef26619f3264b86726c993a
Kevin Smith <kevin341s@yahoo.com>
2023-06-22 15:04:46 -0400

    create collection, send friend request to user
    if status or friends, cant add friend, else send request

--- src/components/addFriend.tsx
+++ src/components/addFriend.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { auth, firestore } from "../lib/firebase.js";
 
-function addFriend() {
-  return <div></div>;
-}
+// sender and receiver are the userIds of the two users
+const addFriend = async (sender, receiver) => {
+  // get reference to the friendship document
+  const friendshipRef = firestore.doc(`friendships/${sender}_${receiver}`);
 
-export default addFriend;
+  // get the friendship document
+  const friendshipDoc = await friendshipRef.get();
+
+  if (friendshipDoc.exists) {
+    const status = friendshipDoc.data().status;
+
+    if (status === "friends" || status === "pending") {
+      // can't send another friend request
+      return;
+    }
+  }
+  // if the code reaches here, it means a friend request can be sent
+  // create or update the friendship document to reflect the new status
+  await friendshipRef.set({
+    status: "pending",
+    sender,
+    receiver,
+  });
+};
