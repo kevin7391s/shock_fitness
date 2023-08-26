4b4e7167f063fb6bd3576b2332f7557d5c6a68ee
Kevin Smith <kevin341s@yahoo.com>
2023-08-26 15:45:55 -0400

    Update friends.tsx
    base template

---
+++ src/pages/friends.tsx
@@ -0,0 +1,9 @@
+import NavBar from "@/components/navbar";
+import Footer from "@/components/footer";
+import React from "react";
+
+function friends() {
+  return <div></div>;
+}
+
+export default friends;
