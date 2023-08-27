2c1ed471b71c57159be12718857da8d142afa44a
Kevin Smith <kevin341s@yahoo.com>
2023-08-27 14:04:00 -0400

    add base template for friend page

--- src/pages/friends.tsx
+++ src/pages/friends.tsx
@@ -3,7 +3,17 @@ import Footer from "@/components/footer";
 import React from "react";
 
 function friends() {
-  return <div></div>;
+  return (
+    <div>
+      <div
+        className="flex flex-col items-center justify-center min-h-screen bg-black text-white"
+        style={{ backgroundColor: "#121212" }}
+      >
+        <NavBar />
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
 export default friends;
